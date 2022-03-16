10240541d73aee71ec0b02cbb72353f2cc3de45d
zenawit.k <zenawit.k@PCDEV01>
2022-03-16 16:14:27 +0300

    InAndOut commited from local for the first time

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+    }
+}
